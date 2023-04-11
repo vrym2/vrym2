3581671bf2c7d99cca9877b6425f77f35fad9ac0
vrym2 <vardhan609@gmail.com>
2023-04-11 12:04:58 +0000

    mamba env update

--- scripts/conda_activate.sh
+++ scripts/conda_activate.sh
@@ -1,14 +1,11 @@
 #!bin/bash
 
 function conda-activate {
-    if [ $(basename $1)=='sar-pre-processing' ]; then
-        echo -e "Activating 'sensarp' conda env"
-        conda activate sensarp
-    elif [ $(basename $1)=='oiltanks' ]; then
+    if [ $(basename $1)=='oiltanks' ]; then
         echo -e "Activating 'oiltanks' conda env"
-        conda activate oiltanks
+        mamba activate esa_snap
     else
         echo "Please check the folder has appropriate conda env"
-        conda info --envs
+        mamba info --envs
     fi
 }
