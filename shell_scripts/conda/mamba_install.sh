1cb06e5c1226323760e82f3cb7a799a91c5dbc6c
vrym2 <vardhan609@gmail.com>
2023-06-15 21:32:54 +0000

    run successful

--- shell_scripts/conda/mamba_install.sh
+++ shell_scripts/conda/mamba_install.sh
@@ -3,7 +3,7 @@
 MAMBA_URL="https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-Linux-x86_64.sh"
 
 # Checking if there is DIR for apps
-if [ -d ~/tmp/apps ]; then
+if [ d ~/tmp/apps ]; then
     echo "Directory exists"
 else
     mkdir -p ~/tmp/apps
@@ -14,5 +14,5 @@ if [ ! d ~/mambaforge ]; then
     wget -P ~/tmp/apps "${MAMBA_URL}"
     bash ~/tmp/apps/Mambaforge-Linux-x86_64.sh
 else
-    mamba activate base
+    mamba init
 fi
