11b53e5d1550a72d9433f7e869606e2e3c8ae5dd
vrym2 <vardhan609@gmail.com>
2023-07-14 13:22:59 +0000

    bug fix

--- shell_scripts/python_path.sh
+++ shell_scripts/python_path.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-# Add folder to python path if it is a .git folder
-parent_dir="$(dirname "$PWD")"
-directories=$(find "$parent_dir" -type d)
-for dir in $directories; do
-    if [[ ":PYTHONPATH:" != *":$dir:"* ]]; then
-        echo "Adding $dir to the PYTHONPATH"
-        export PYTHONPATH="$dir:$PYTHONPATH"
-    fi
-done
+# Add folder to python path
+if [[ ":PYTHONPATH:" != *":$PWD:"* ]]; then
+    echo "Adding $PWD to the PYTHONPATH"
+    export PYTHONPATH="$PWD:$PYTHONPATH"
+fi
 
